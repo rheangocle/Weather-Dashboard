fef473086a5b8bd3598e4d474ca58f2f44e840b3
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-07-05 19:23:15 -0400

    changed code functions so that values that are gathered in one function gets plugged into another, this made the page update automatically when user searches for a new city

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,16 +13,17 @@ THEN I am again presented with current and future conditions for that city
 */
 //Declare variables
 const apiKey = 'dab6c26050c6340b4ec6ae2e98ef076c';
-let city = $('.city-input').val().trim();
-const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
 var todayWeather = $('.weather-today');
 var today = moment().format('MM/DD/YY');
 var fiveDayEl = $('.five-day-container');
-var searchListEl = $('.city-searches');
+var searchListEl = $('.list-group');
 
-function getToday() {
+function getToday(city) {
   //Display today's weather
   // event.preventDefault();
+  const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
   fetch(queryURL, {
     method: 'GET',
   })
@@ -34,7 +35,7 @@ function getToday() {
       //console.log(data);
 
       var currentCityInfo = `
-      <h2>${data.name} (${today}) <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt='weather icon' width='70' height='70'></h2>
+      <h2>${data.name} (${today}) <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt='weather icon'></h2>
       <p>🌡 Temperature: ${Math.round(((data.main.temp - 273.15) * 9 / 5) + 32)} ºF</p>
       <p>💧 Humidity: ${data.main.humidity}%</p>
       <p>💨 Wind speed: ${data.wind.speed} mph</p>`;
@@ -43,7 +44,7 @@ function getToday() {
       todayWeather.html(currentCityInfo);
       var latitude = data.coord.lat;
       var longitude = data.coord.lon;
-
+      getForecast(latitude, longitude);
       //Getting the UV Index information for current day
       var uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 
@@ -74,111 +75,59 @@ function getToday() {
     })
 }
 
-function getForecast() {
+function getForecast(latitude, longitude) {
   // event.preventDefault();
-  var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${apiKey}`;
-  var latLon = [];
+  // var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${apiKey}`;
+  // var latLon = [];
 
-  fetch(geoURL, {
+  // fetch(geoURL, {
+  //   method: 'GET',
+  // })
+  //   .then(function (response) {
+  //     //console.log(response)
+  //     return response.json();
+  //   })
+  //   .then(function (latLonData) {
+  //     //console.log(latLonData);
+  //     var arr = latLonData
+  //     latLon.push(arr[0].lat);
+  //     latLon.push(arr[0].lon);
+  //     //console.log(latLon)
+
+  //Five day forecast
+  var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`
+  // `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0]}&lon=${latLon[1]}&units=imperial&appid=${apiKey}`
+
+  fetch(fiveDayUrl, {
     method: 'GET',
   })
     .then(function (response) {
-      //console.log(response)
+      console.log(response)
       return response.json();
     })
-    .then(function (latLonData) {
-      //console.log(latLonData);
-      var arr = latLonData
-      latLon.push(arr[0].lat);
-      latLon.push(arr[0].lon);
-      //console.log(latLon)
+    .then(function (data) {
+      console.log(data);
+      //changed from append to html so that the info would not repopulate on page after every submit button
+      fiveDayEl.html(`<h3>5 Day Forecast</h3>`);
 
-      //Five day forecast
-      var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon[0]}&lon=${latLon[1]}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`
-      // `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0]}&lon=${latLon[1]}&units=imperial&appid=${apiKey}`
-
-      fetch(fiveDayUrl, {
-        method: 'GET',
-      })
-        .then(function (response) {
-          console.log(response)
-          return response.json();
-        })
-        .then(function (data) {
-          console.log(data);
-          //changed from append to html so that the info would not repopulate on page after every submit button
-          fiveDayEl.html(`<h3>5 Day Forecast</h3>`);
-
-          for (var i = 1; i < data.daily.length - 2; i++) {
-            var dayForecast = `
+      for (var i = 1; i < data.daily.length - 2; i++) {
+        var dayForecast = `
             <div class="card card-body forecast-card col-2">
               <h5 class="card-title">${moment.unix(data.daily[i].dt).format('MM/DD/YY')}</h5>
-              <img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png" alt='weather icon' width='90' height='90'>
+              <img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" alt='weather icon'>
               <p class="card-text">Temperature: ${Math.round(data.daily[i].temp.day)} ºF</p>
               <p class="card-text">Wind speed: ${data.daily[i].wind_speed} mph</p>
               <p class="card-text">Humidity: ${data.daily[i].humidity} %</p>
             </div>`
 
-            fiveDayEl.append(dayForecast);
-            console.log(fiveDayEl)
-          }
-
-          // var firstDayForecast = `
-          //   <div class="card card-body forecast-card col-2">
-          //     <h5 class="card-title">${moment.unix(data.daily[1].dt).format('MM/DD/YY')}</h5>
-          //     <img src="https://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}.png" alt='weather icon' width='80' height='80'>
-          //     <p class="card-text">Temperature: ${Math.round(data.daily[1].temp.day)} ºF</p>
-          //     <p class="card-text">Wind speed: ${data.daily[1].wind_speed} mph</p>
-          //     <p class="card-text">Humidity: ${data.daily[1].humidity} %</p>
-          //   </div>`
-          // //console.log(`${moment.unix(data.daily[1].dt).format('MM/DD/YY')}`);
-
-          // fiveDayEl.append(firstDayForecast);
-          // var secondDayForecast = `
-          //   <div class="card card-body forecast-card col-2">
-          //     <h5 class="card-title">${moment.unix(data.daily[2].dt).format('MM/DD/YY')}</h5>
-          //     <img src="https://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}.png" alt='weather icon' width='80' height='80'>
-          //     <p class="card-text">Temperature: ${Math.round(data.daily[2].temp.day)} ºF</p>
-          //     <p class="card-text">Wind speed: ${data.daily[2].wind_speed} mph</p>
-          //     <p class="card-text">Humidity: ${data.daily[2].humidity} %</p>
-          //   </div>`
-
-          // fiveDayEl.append(secondDayForecast);
-
-          // var thirdDayForecast = `
-          //   <div class="card card-body forecast-card col-2">
-          //     <h5 class="card-title">${moment.unix(data.daily[3].dt).format('MM/DD/YY')}</h5>
-          //     <img src="https://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}.png" alt='weather icon' width='80' height='80'>
-          //     <p class="card-text">Temperature: ${Math.round(data.daily[3].temp.day)} ºF</p>
-          //     <p class="card-text">Wind speed: ${data.daily[3].wind_speed} mph</p>
-          //     <p class="card-text">Humidity: ${data.daily[3].humidity} %</p>
-          //   </div>`
-
-          // fiveDayEl.append(thirdDayForecast);
-          // var fourDayForecast = `
-          //   <div class="card card-body forecast-card col-2">
-          //     <h5 class="card-title">${moment.unix(data.daily[4].dt).format('MM/DD/YY')}</h5>
-          //     <img src="https://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}.png" alt='weather icon' width='80' height='80'>
-          //     <p class="card-text">Temperature: ${Math.round(data.daily[4].temp.day)} ºF</p>
-          //     <p class="card-text">Wind speed: ${data.daily[4].wind_speed} mph</p>
-          //     <p class="card-text">Humidity: ${data.daily[4].humidity} %</p>
-          //   </div>`
-
-          // fiveDayEl.append(fourDayForecast);
-          // var fiveDayForecast = `
-          //   <div class="card card-body forecast-card col-2">
-          //     <h5 class="card-title">${moment.unix(data.daily[5].dt).format('MM/DD/YY')}</h5>
-          //     <img src="https://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}.png" alt='weather icon' width='80' height='80'>
-          //     <p class="card-text">Temperature: ${Math.round(data.daily[5].temp.day)} ºF</p>
-          //     <p class="card-text">Wind speed: ${data.daily[5].wind_speed} mph</p>
-          //     <p class="card-text">Humidity: ${data.daily[5].humidity} %</p>
-          //   </div>`
-
-          // fiveDayEl.append(fiveDayForecast);
-        })
+        fiveDayEl.append(dayForecast);
+        console.log(fiveDayEl)
+      }
 
     })
+
 }
+
 
 //Set up local storage
 
@@ -190,17 +139,18 @@ function citySearch(e) {
   var cityArr = [];
   // cityArr.push(city);
   // console.log(searchHistory);
-  getToday();
-  getForecast();
+  let city = $('.city-input').val().trim();
+  getToday(city);
+
+
   if (!cityArr.includes(city)) {
+    //might remove some of these classes
     var cityList = `
-    <div class="list-group">
       <button type="button" class="list-group-item list-group-item-action active prev-cities"
         aria-current="true">
         ${city}
-      </button>
-    </div>`
-    searchListEl.append(cityList);
+      </button>`
+    searchListEl.html(cityList); //append or html here??
     cityArr.push(city);
   }
   //need to add code to check if city is duplicate and remove it, need to add list of searched cities to local storage
